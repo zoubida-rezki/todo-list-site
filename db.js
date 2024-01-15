b84cc24ff4698967b790e845361714e86ea20e06
zoubida-rezki <zoubidaziroki@gmail.com>
2024-01-15 17:22:00 -0600

    first commit

---
+++ db.js
@@ -0,0 +1,13 @@
+//conecting to our database
+const {Pool} = require('pg'); // to conect databases to servers
+
+const pool = new Pool({
+    user:"postgres",
+    password:'z',
+    host:'localhost',
+    port:5432,
+    database:'todo'
+});
+
+module.exports = pool;
+
